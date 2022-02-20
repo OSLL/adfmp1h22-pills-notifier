e52211258c0f276c3f41f47debf7cdeef6dd4a9f
lsunko <sunko.elena2001@gmail.com>
2022-02-20 12:02:51 +0300

    add server models

---
+++ server/models/medicine_info.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+
+
+class Regularity(Enum):
+    DAILY = 1
+    ONCE_IN_TWO_DAYS = 2
+    ONCE_A_WEEK = 3
+
+
+@dataclass
+class MedicineInfo:
+    medicine_name: str
+    instructions: str
+    regularity: Regularity
+    start_date: datetime
+    time: datetime
